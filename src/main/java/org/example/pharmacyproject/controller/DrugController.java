5824db605ae7778c70bd679a99f00c8ca620c4a3
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-04-08 22:16:01 +0200

    create Dtos, DrugService, DrugRepository, DrugEntity, DrugController, CreateDrugResponseDto, CreareDrugDto

---
+++ src/main/java/org/example/pharmacyproject/controller/DrugController.java
@@ -0,0 +1,48 @@
+package org.example.pharmacyproject.controller;
+
+import org.example.pharmacyproject.controller.dto.CreateDrugDto;
+import org.example.pharmacyproject.controller.dto.CreateDrugResponseDto;
+import org.example.pharmacyproject.controller.dto.GetDrugDto;
+import org.example.pharmacyproject.infrastructure.entity.DrugEntity;
+import org.example.pharmacyproject.service.DrugService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/drugs")
+public class DrugController {
+
+    private final DrugService drugService;
+
+    @Autowired
+    public DrugController(DrugService drugService) {
+        this.drugService = drugService;
+    }
+
+    @GetMapping
+    public List<GetDrugDto> getAllDrugs(){
+        return drugService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public GetDrugDto getOne(@PathVariable long id){
+        return drugService.getOne(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateDrugResponseDto> create(@RequestBody CreateDrugDto drug){
+        var newDrug = drugService.create(drug);
+        return new ResponseEntity<>(newDrug, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        drugService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/org/example/pharmacyproject/controller/dto/CreateDrugDto.java
@@ -0,0 +1,70 @@
+package org.example.pharmacyproject.controller.dto;
+
+public class CreateDrugDto {
+    private String name;
+    private String manufacturer;
+    private String description;
+    private String dosage;
+    private String type;
+    private boolean prescriptionRequired;
+
+    public CreateDrugDto(String name, String manufacturer, String description, String dosage, String type, boolean prescriptionRequired) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.description = description;
+        this.dosage = dosage;
+        this.type = type;
+        this.prescriptionRequired = prescriptionRequired;
+    }
+
+    public CreateDrugDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isPrescriptionRequired() {
+        return prescriptionRequired;
+    }
+
+    public void setPrescriptionRequired(boolean prescriptionRequired) {
+        this.prescriptionRequired = prescriptionRequired;
+    }
+}

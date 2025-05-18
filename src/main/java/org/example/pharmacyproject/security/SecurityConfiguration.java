4d1355450e592e0eafe647e974191ee0da6dc381
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-05-18 09:27:05 +0200

    pre checkout commit

--- src/main/java/org/example/pharmacyproject/security/SecurityConfiguration.java
+++ src/main/java/org/example/pharmacyproject/security/SecurityConfiguration.java
@@ -1,10 +1,7 @@
 package org.example.pharmacyproject.security;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
